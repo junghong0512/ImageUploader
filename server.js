f59a5001374608d2dffe792959e1e77366691cdc
Jung Hong <junghong0512@gmail.com>
2020-04-14 11:48:09 +0900

    server.js of uploading part is done

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
@@ -9,6 +10,24 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
+
+//Upload Endpoint
+app.post("/upload", (req, res) => {
+  if (req.file === null) {
+    return res.status(400).json();
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send(err);
+    }
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
 
 const uri = process.env.ATLAS_URI;
 
